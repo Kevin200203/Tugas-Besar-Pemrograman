48a44d6e459579c1a5aa76553256c57c901fb876
kevin200203 <kevinsiringo756@gmail.com>
2021-11-29 22:32:21 +0700

    Mengganti file tugas besar pemrograman.c

--- Tugas Besar Pemrograman.c
+++ Tugas Besar Pemrograman.c
@@ -1,10 +1,19 @@
 /*****************************************************************************************************
 						Tugas Besar Pemrograman
 TANGGAL PEMBUATAN 11/23/2021
-OLEH :  -KEVIN SIRINGORINGO
-		-I Gede Ari Wisnu Sanjaya
+OLEH :
+        -KEVIN SIRINGORINGO
+        -I GEDE ARI WISNU SANJAYA
 REVISI : 0
-DESKRIPSI : Membuat Game Windows Controller 
+DESKRIPSI : Program Pembuatan Game Windows console Tic Tac Toe
 ******************************************************************************************************/
 
-#include <stdio>
+//Memasukkan Library yang digunakan dalam pembuatan game Tic Tac Toe
+#include <stdio.h> //  digunakan untuk input output
+#include <conio.h> //  digunakan untuk menampilkan hasil antar muka kepada pengguna
+#include <windows.h> //  berisi deklarasi untuk semua fungsi di windows API
+#include <stdlib.h>  //   merupakan header file untuk standar library
+#include <MMsystem.h> // merupakan library yang menyediakan serangkaian fungsi fungsi yang terkait dengan sistem operasi
+
+
+
