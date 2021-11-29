ec151239e3f87f68df11ee8563375545c5663a0e
Kevin Siringoringo <90599069+Kevin200203@users.noreply.github.com>
2021-11-29 15:31:27 +0700

    Add files via upload

---
+++ Tugas Besar Pemrograman.c
@@ -0,0 +1,10 @@
+/*****************************************************************************************************
+						Tugas Besar Pemrograman
+TANGGAL PEMBUATAN 11/23/2021
+OLEH :  -KEVIN SIRINGORINGO
+		-I Gede Ari Wisnu Sanjaya
+REVISI : 0
+DESKRIPSI : Membuat Game Windows Controller 
+******************************************************************************************************/
+
+#include <stdio>
