3eab7eac4063e8980086687c577f0c3ae114dc63
dewisnu <wisnuari4658@gmail.com>
2021-12-12 03:16:44 +0800

    menambahkan baris kode
    membuat fungsi cekMenang

--- ticTacToe/function.h
+++ ticTacToe/function.h
@@ -15,3 +15,27 @@ void menampilkanPapan()
     printf("  %c  |  %c  |  %c  \n", kotak[7], kotak[8], kotak[9]);
     printf("     |     |     \n");
 }
+
+int cekMenang()
+{
+    if (kotak[1] == kotak[2] && kotak[2] == kotak[3])
+        return 1;
+    else if (kotak[4] == kotak[5] && kotak[5] == kotak[6])
+        return 1;
+    else if (kotak[7] == kotak[8] && kotak[8] == kotak[9])
+        return 1;
+    else if (kotak[1] == kotak[4] && kotak[4] == kotak[7])
+        return 1;
+    else if (kotak[2] == kotak[5] && kotak[5] == kotak[8])
+        return 1;
+    else if (kotak[3] == kotak[6] && kotak[6] == kotak[9])
+        return 1;
+    else if (kotak[1] == kotak[5] && kotak[5] == kotak[9])
+        return 1;
+    else if (kotak[3] == kotak[5] && kotak[5] == kotak[7])
+        return 1;
+    else if (kotak[1] != '1' && kotak[2] != '2' && kotak[3] != '3' && kotak[4] != '4' && kotak[5] != '5' && kotak[6] != '6' && kotak[7] != '7' && kotak[8] != '8' && kotak[9] != '9')
+        return 0;
+    else
+        return -1;
+}
