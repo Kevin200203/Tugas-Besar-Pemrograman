66ac95bf386e16721fe29b830fc8b6a14b4453cd
dewisnu <wisnuari4658@gmail.com>
2021-12-12 03:52:40 +0800

    menambahkan baris kode

--- ticTacToe/ticTacToe.c
+++ ticTacToe/ticTacToe.c
@@ -5,5 +5,62 @@
 #include <mmsystem.h>
 #include "function.h"
 
+char kotak[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int cekMenang();
 void menampilkanPapan();
+
+int main()
+{
+    system("color 78");
+    int pemain = 1, i, pilihan;
+    char tanda; //X,O
+    do
+    {
+
+        menampilkanPapan();
+        pemain = (pemain % 2) ? 1 : 2;
+        printf("Pemain %d, memasukan pilihan : ", pemain);
+        scanf("%d", &pilihan);
+        tanda = (pemain == 1) ? 'X' : 'O';
+        if (pilihan == 1 && kotak[1] == '1')
+            kotak[1] = tanda;
+        else if (pilihan == 2 && kotak[2] == '2')
+            kotak[2] = tanda;
+        else if (pilihan == 3 && kotak[3] == '3')
+            kotak[3] = tanda;
+        else if (pilihan == 4 && kotak[4] == '4')
+            kotak[4] = tanda;
+        else if (pilihan == 5 && kotak[5] == '5')
+            kotak[5] = tanda;
+        else if (pilihan == 6 && kotak[6] == '6')
+            kotak[6] = tanda;
+        else if (pilihan == 7 && kotak[7] == '7')
+            kotak[7] = tanda;
+        else if (pilihan == 8 && kotak[8] == '8')
+            kotak[8] = tanda;
+        else if (pilihan == 9 && kotak[9] == '9')
+            kotak[9] = tanda;
+
+        else
+        {
+            printf("pilihan salah !");
+            pemain--;
+            getch();
+        }
+        i = cekMenang();
+        pemain++;
+    } while (i == -1);
+
+    menampilkanPapan();
+    if (1 == 1)
+    {
+        printf("==>pemain %d menang", --pemain);
+        
+    }
+    else
+    {
+        printf("==> permainan seri");
+    }
+    getch();
+    return 0;
+}
